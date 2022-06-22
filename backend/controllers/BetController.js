a5d612899bba0c67c10ada310e052180bc7a85dd
Razo <raz.miqayelyan@gmail.com>
2022-06-22 11:34:34 +0400

    make it better

--- backend/controllers/BetController.js
+++ backend/controllers/BetController.js
@@ -3,18 +3,45 @@ import { data } from "../config/db.js"
 
 export const Prediction = async(req, res) => {
     try {
+        // const {playerPrediction, positionID, bank} = req.body
+        // const myBank = parseInt(bank)
+        // const myPositionID = parseInt(positionID)
+        // const myPlayerPrediction = parseFloat(playerPrediction)
+        // if(playerPrediction >= 1) return res.status(404).send("Player Prediction cannot be grather or equal 1")
+        // if (!playerPrediction || isNaN(playerPrediction)) return res.status(400).send("Uncorrect Value for Player Prediction")
+        // const myCoefficient = await data.promise().query("SELECT  Positions.coefficient FROM Positions WHERE id=?", [positionID])
+        // if(!myCoefficient[0][0]) return res.status(404).send("Coefficient Undefined")
+        // // res.send(myCoefficient[0][0])
+        // const passive_1 = parseInt(((myCoefficient[0][0].coefficient * parseFloat(playerPrediction) - 1) / (myCoefficient[0][0].coefficient - 1)* parseInt(bank)))
+        // const passive_2 = parseInt((((myCoefficient[0][0].coefficient - 0.01) * parseFloat(playerPrediction) - 1) / ((myCoefficient[0][0].coefficient - 0.01) - 1) * parseInt(bank)))
+        // const passive = parseInt(passive_2 + ((passive_1 - passive_2) * 0.7))
+        // if(passive < parseInt(bank) ) return res.send({passive:"Too Risky Bet"}) 
+        // // res.send({passive:parseInt(((passive_2) + ((passive_1 - passive_2) * 0.7)))})
+
+        // // change passive name to passive
+        // res.send({passive})   
+
         const {playerPrediction, positionID, bank} = req.body
+        
+        let status = 'OK'
+        const myBank = parseInt(bank)
+        const myPositionID = parseInt(positionID)
+        const myPlayerPrediction = parseFloat(playerPrediction)
         if(playerPrediction >= 1) return res.status(404).send("Player Prediction cannot be grather or equal 1")
         if (!playerPrediction || isNaN(playerPrediction)) return res.status(400).send("Uncorrect Value for Player Prediction")
-        const myCoefficient = await data.promise().query("SELECT  Positions.coefficient FROM Positions WHERE id=?", [positionID])
+        const myCoefficient = await data.promise().query("SELECT  Positions.coefficient FROM Positions WHERE id=?", [myPositionID])
         if(!myCoefficient[0][0]) return res.status(404).send("Coefficient Undefined")
         // res.send(myCoefficient[0][0])
-        const optimal_1 = parseInt(((myCoefficient[0][0].coefficient * parseFloat(playerPrediction) - 1) / (myCoefficient[0][0].coefficient - 1)* parseInt(bank)))
-        const optimal_2 = parseInt((((myCoefficient[0][0].coefficient - 0.01) * parseFloat(playerPrediction) - 1) / ((myCoefficient[0][0].coefficient - 0.01) - 1) * parseInt(bank)))
-        const optimal = parseInt(optimal_2 + ((optimal_1 - optimal_2) * 0.7))
-        if(optimal < 0 ) return res.send({optimal:"Too Risky Bet"}) 
-        // res.send({optimal:parseInt(((optimal_2) + ((optimal_1 - optimal_2) * 0.7)))})
-        res.send({optimal})   
+        const passive_1 = parseInt(((myCoefficient[0][0].coefficient * myPlayerPrediction - 1) / (myCoefficient[0][0].coefficient - 1)* myBank))
+        const passive_2 = parseInt((((myCoefficient[0][0].coefficient - 0.01) * myPlayerPrediction - 1) / ((myCoefficient[0][0].coefficient - 0.01) - 1) * myBank))
+        const passive = parseInt(passive_2 + ((passive_1 - passive_2) * 0.7))
+        // if(passive < myBank ) return res.send({passive:"Too Risky Bet"}) 
+        if(passive < myBank * 0.04) status = "Too Risky Bet"
+        else if (passive > myBank * 0.1) status = 'Add More Games'
+        // res.send({passive:parseInt(((passive_2) + ((passive_1 - passive_2) * 0.7)))})
+        // change passive name to passive
+        res.send({passive, status})   
+
     } catch (error) {
         res.status(400).send(error.message)
     }
